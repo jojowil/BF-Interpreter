f5cec32aa66f67ae6c651657a65a0a4e9a23fda1
Bill Jojo <jojowil@gmail.com>
2024-12-31 19:23:23 -0500

    the 6502 version assembles and runs, but doesn't finish processing hello3.b. committing this anyway.

--- src/bf-6502.asm
+++ src/bf-6502.asm
@@ -2,6 +2,11 @@
 ; in the 6502js web-based emulator.
 
 ;=========================
+; I/O
+define CHROUT $FFD2
+define CHRIN  $FFCF
+
+;=========================
 ; give arbitrary names to the cmds
 define LESS  $3C ; < $3c
 define GRTR  $3E ; > $3e
@@ -12,6 +17,8 @@ define MINUS $2D ; - $2d
 define OPEN  $5B ; [ $5b
 define CLOSE $5D ; ] $5d
 
+;=========================
+; define our code and arena pointers
 define CPLO $20 ; $20/21 for code pointer
 define CPHI $21
 define APLO $30 ; $30/31 for arena pointer
@@ -36,9 +43,46 @@ mainend:
 ; Subroutines below this line
 ;
 
+;=========================
 ; interpret a command.
 interpret:
-
+    TAX     ; save the cmd
+    CMP #GRTR
+    BNE mnext1
+    JMP incap
+mnext1:
+    CMP #LESS
+    BNE mnext2
+    JMP decap
+mnext2:
+    CMP #PLUS
+    BNE mnext3
+    JMP appp
+mnext3:
+    CMP #MINUS
+    BNE mnext4
+    JMP apmm
+mnext4:
+    CMP #DOT
+    BNE mnext5
+    JMP apin
+mnext5:
+    CMP #COMMA
+    BNE mnext6
+    JMP apin
+mnext6:
+    CMP #OPEN
+    LDY #0
+    LDA (APLO), Y
+    BNE mnext7
+    JMP matchc
+mnext7:
+    TXA     ; retrieve saved!
+    CMP #CLOSE
+    LDY #0  ; prolly redundant
+    LDA (APLO), Y
+    BEQ idone
+    JMP matcho
 idone:
     RTS
 
@@ -89,26 +133,79 @@ apskip:
 
 ;=========================
 appp:
+    LDY #0
+    LDA (APLO), Y
+    TAX
+    INX
+    TXA
+    STA (APLO), Y
     RTS
 
 ;=========================
 apmm:
+    LDY #0
+    LDA (APLO), Y
+    TAX
+    DEX
+    TXA
+    STA (APLO), Y
     RTS
 
 ;=========================
 apout:
+    LDY #0
+    LDA (APLO), Y
+    JSR CHROUT
     RTS
 
 ;=========================
 apin:
+    JSR CHRIN
+    CMP #0
+    BEQ apin
+    LDY #0
+    STA (APLO), Y
     RTS
 
 ;=========================
+; find maching open
 matcho:
+    LDX #1
+oloop:
+    JSR deccp
+    CPX #0      ; >255 ] would be an issue. ;-)
+    BEQ odone
+    JSR getcmd
+    CMP #CLOSE
+    BNE onext
+    INX
+    JMP oloop
+onext:
+    CMP #OPEN
+    BNE oloop
+    DEX
+    JMP oloop
+odone:
     RTS
 
 ;=========================
 matchc:
+    LDX #1
+cloop:
+    JSR inccp
+    CPX #0      ; >255 ] would be an issue. ;-)
+    BEQ cdone
+    JSR getcmd
+    CMP #OPEN
+    BNE cnext
+    INX
+    JMP cloop
+cnext:
+    CMP #CLOSE
+    BNE cloop
+    DEX
+    JMP cloop
+cdone:
     RTS
 
 ;=========================
@@ -131,8 +228,9 @@ init:
 ;=========================
 ; source code for BF program
 code:
-    TXT ""
-    TXT ""
+    ; taken from hello3.v
+    TXT "+++++++++++[>++++++>+++++++++>++++++++>++++>+++>+<<<<<<-]>+++"
+    TXT "+++.>++.+++++++..+++.>>.>-.<<-.<.+++.------.--------.>>>+.>-."
     dcb 0 ; end of program
 
 ;=========================
